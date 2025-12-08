8c9f5c28c27716b6b49fe0dae49a16b6e150c315
tydapchai <ty3072004@gmail.com>
2025-12-08 14:33:43 +0700

    get me comment liked

--- apps/api/src/handlers/users/index.ts
+++ apps/api/src/handlers/users/index.ts
@@ -4,6 +4,7 @@ export { handler as getMyReviewsHandler } from './reviews';
 export { handler as getMySavedHandler } from './saved';
 export { handler as getMyStatsHandler } from './stats';
 export { handler as getMyVotesHandler } from './votes';
+export { handler as getMyLikedCommentsHandler } from './liked-comments';
 export { getUploadUrlHandler as getAvatarUploadUrlHandler, updateAvatarHandler } from './avatar';
 export { getBackgroundUploadUrlHandler, updateBackgroundHandler } from './background';
 export { setPasswordHandler } from './set-password';
